db438eb9cd8f7e8cebf9ab5e8b3f7024ad4d6af5
Kevin Delisle <kjdelisle@gmail.com>
2018-03-21 08:52:41 -0400

    chore: update nvm to 0.33.8

--- nvm-setup.sh
+++ nvm-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # nvm
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh > nvm-install.sh
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh > nvm-install.sh
 chmod +x nvm-install.sh
 ./nvm-install.sh
 
